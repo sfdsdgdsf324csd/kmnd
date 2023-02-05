824a0456a8373478dff07f7a023d21a64f2d103d
sfdsdgdsf324csd <116049211+sfdsdgdsf324csd@users.noreply.github.com>
2023-02-05 09:37:51 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # 定义 UUID 及伪装路径、哪吒面板参数，请自行修改. (注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
-UUID='de04add9-5c68-8bab-950c-08cd5320df18'
-VMESS_WSPATH='/vmess'
-VLESS_WSPATH='/vless'
-TROJAN_WSPATH='/trojan'
+UUID='b0941a6a-9e04-4fbb-825b-1af26d149d3d'
+VMESS_WSPATH='/b0941a6a-9e04-4fbb-825b-1af26d149d3d-vmess'
+VLESS_WSPATH='/b0941a6a-9e04-4fbb-825b-1af26d149d3d-vless'
+TROJAN_WSPATH='/b0941a6a-9e04-4fbb-825b-1af26d149d3d-trojan'
 SS_WSPATH='/shadowsocks'
 NEZHA_SERVER=''
 NEZHA_PORT=''
